b40443f1de3428b6211baa01ff25a32d7f019c85
Raghuveer47 <122000888+Raghuveer47@users.noreply.github.com>
2023-12-30 16:49:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const app = express();
 app.use((req, res, next) => {
-  const allowedOrigin = 'https://fpay-back.onrender.com';
+  const allowedOrigin = 'https://sprightly-gumdrop-c46663.netlify.app';
   res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
