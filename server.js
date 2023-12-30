e629a9c357d4f583352422da4102f5c178c9c8cb
Raghuveer47 <122000888+Raghuveer47@users.noreply.github.com>
2023-12-30 12:35:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const app = express();
 app.use((req, res, next) => {
-  const allowedOrigin = 'https://curious-platypus-651cab.netlify.app';
+  const allowedOrigin = 'https://sprightly-gumdrop-c46663.netlify.app';
   res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
